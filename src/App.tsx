3fb93a604c871ebae9c0e4908b67baf037ef9a0c
Ramzi Hizi <rmh@rmhPro16.local>
2024-01-05 09:39:19 -0500

    build: add tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Credit Card Luhn checkSum</h1>
-      </div>
-    </>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Credit Card Luhn checkSum
+      </h1>
+    </div>
   );
 }
 
